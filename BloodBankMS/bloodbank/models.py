cab1939dcf9e408cf871e553831cc20ff2ff38de
Farhana islam urmi <76726981+Farhanaurmi@users.noreply.github.com>
2021-02-28 22:17:48 +0600

    models,admin edit

--- BloodBankMS/bloodbank/models.py
+++ BloodBankMS/bloodbank/models.py
@@ -1,3 +1,17 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class BloodClass(models.Model):
+    name = models.CharField(max_length=100)
+    blood_group = models.CharField(max_length=3)
+    age = models.IntegerField(max_length=3)
+    address = models.CharField(max_length=250)
+    phone_number = models.IntegerField(max_length=11)
+
+    user = models.ForeignKey(User,on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.name
+
+
+
