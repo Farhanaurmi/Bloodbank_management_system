99802e95fd3eba141d9148a88b4c277d7d33e842
Mahadi Hasan Tanmoy <53210886+mhtanmoy@users.noreply.github.com>
2021-02-28 21:51:24 +0600

    m.2

--- BloodBankMS/BloodBankMS/urls.py
+++ BloodBankMS/BloodBankMS/urls.py
@@ -23,4 +23,5 @@ urlpatterns = [
     path('signup/', views.signupuser, name='signupuser'),
     path('login/', views.loginuser, name='loginuser'),
     path('logout/', views.logoutuser, name='logoutuser'),
+    
 ]
