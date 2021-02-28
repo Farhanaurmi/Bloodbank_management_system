42237908514979d914156e9bc5311faf2e45408f
Farhana islam urmi <76726981+Farhanaurmi@users.noreply.github.com>
2021-02-28 20:58:15 +0600

    signup,base.html

--- BloodBankMS/BloodBankMS/urls.py
+++ BloodBankMS/BloodBankMS/urls.py
@@ -20,4 +20,5 @@ from bloodbank import views
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.home, name='home'),
+    path('signup/', views.signupuser, name='signupuser'),
 ]
--- BloodBankMS/bloodbank/views.py
+++ BloodBankMS/bloodbank/views.py
@@ -1,5 +1,24 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.models import User
+from django.db import IntegrityError
+from django.contrib.auth import login
 
 # Create your views here.
 def home(request):
     return render(request, 'bloodbank/home.html')
+
+def signupuser(request):
+    if request.method=='GET':
+        return render(request, 'bloodbank/signupuser.html',{'form':UserCreationForm})
+    else:
+        if request.POST['password1']==request.POST['password2']:
+            try:
+                user=User.objects.create_user(request.POST['username'],password=request.POST['password1'])
+                user.save()
+                login(request,user)
+                return redirect('home')
+            except IntegrityError:
+                return render(request, 'bloodbank/signupuser.html', {'form':UserCreationForm,'error':'username has been already taken'})
+        else:
+            return render(request, 'bloodbank/signupuser.html', {'form':UserCreationForm,'error':'password did not match'})
